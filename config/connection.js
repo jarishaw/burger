87d7635abcd5b0f60f55f28f02147b65be5639b2
jarishaw <jarishaw@gmail.com>
2018-03-05 14:21:40 -0600

    add img for read me

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "Code17",
+        password: "root",
         database: "burgers_db"
     });
 }; 
